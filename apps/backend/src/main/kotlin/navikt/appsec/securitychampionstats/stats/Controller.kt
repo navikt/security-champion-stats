93180c718ba0d511ff7545db3888170ac562813d
paulius <paulius.deveika@nav.no>
2025-12-01 12:48:02 +0100

    fix: zoom get path, body is not allowed

--- apps/backend/src/main/kotlin/navikt/appsec/securitychampionstats/stats/Controller.kt
+++ apps/backend/src/main/kotlin/navikt/appsec/securitychampionstats/stats/Controller.kt
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -80,8 +81,8 @@ class Controller(
         repo.addPoints(member.id, points)
     }
 
-    @GetMapping("/zoomTest")
-    fun zoomTest(@RequestBody meetingId: String) {
+    @GetMapping("/zoomTest/{meetingId}")
+    fun zoomTest(@PathVariable meetingId: String) {
         val participants = zoomService.getLiveParticipants(meetingId)
         logger.info("Zoom participants: $participants")
         val member = participants.participants?.filter {

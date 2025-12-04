2c8d430c0b08becd0aecb85442f1adaf81cd7707
paulius <paulius.deveika@nav.no>
2025-12-04 13:08:12 +0100

    fix: headers in teamcatlog config

--- apps/backend/src/main/kotlin/navikt/appsec/securitychampionstats/common/teamCatalog/TeamCatalogConfig.kt
+++ apps/backend/src/main/kotlin/navikt/appsec/securitychampionstats/common/teamCatalog/TeamCatalogConfig.kt
@@ -2,23 +2,20 @@ package navikt.appsec.securitychampionstats.common.teamCatalog
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpHeaders
 import org.springframework.util.unit.DataSize
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class TeamCatalogConfig() {
-    //private val size = DataSize.ofMegabytes(16).toBytes().toInt()
+    private val size = DataSize.ofMegabytes(16).toBytes().toInt()
     private val url = "http://team-catalog-backend.org.svc.cluster.local"
     @Bean
     fun externalServiceWebClient(builder: WebClient.Builder): WebClient =
         builder
             .baseUrl(url)
-            .defaultHeaders { headers ->
-                headers.add("accept", "json")
-                headers.add(HttpHeaders.CONTENT_TYPE, "json")
-                headers.add(HttpHeaders.USER_AGENT, "NAV IT McBotFace")
+            .codecs { codecs ->
+                codecs.defaultCodecs().maxInMemorySize(size)
             }
-
+            .defaultHeader("accept", "*/*")
             .build()
 }

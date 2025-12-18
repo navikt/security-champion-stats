a175053cea718904283a4566860537c2abc21d97
paulius <paulius.deveika@nav.no>
2025-12-18 13:23:20 +0100

    feat: build and deploy frontend, i hope

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': "http://127.0.0.1:8000"
+  ...(mode == 'development' && {
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': "http://127.0.0.1:8000",
+      }
     }
-  },
+  }),
   build: {
     outDir: 'dist'
   }
-})
+}))
